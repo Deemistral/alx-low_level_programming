d6d0e53dd54fe4278017598c286f52b99e2d829f
Deemistral <olaniyiadedolapo@gmail.com>
2022-09-21 16:20:20 -0700

    retry1

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string including the terminating null byte
+ *
+ * @dest: a buffer storing the string copy
+ * @src: the source string
+ * @n: max number of byte copied
+ * Return: return value
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
