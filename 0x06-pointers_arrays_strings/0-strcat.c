2510e4f2a4a6f62b69f583c9b716fdb4e353c742
Deemistral <olaniyiadedolapo@gmail.com>
2022-09-21 16:53:11 -0700

    fix1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,17 +11,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index =, dest_len = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (i = 0; src[i] != '\0' ; i++)
-	{
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
-		
-
-	}
 
 
 	return (dest);
